bfb60ff1286e71eec458b64f522b74190a79f87c
omartalibi <omar.talibi.96@gmail.com>
2018-01-17 20:30:06 +0100

    @Creating Test
    typical_common_year_when_not_divisble_by_4

--- src/kata/leapyear/LeapYearShould.java
+++ src/kata/leapyear/LeapYearShould.java
@@ -9,4 +9,8 @@ public class LeapYearShould {
     public void leap_year_when_divisible_by_4_and_neither_divisible_by_100_nor_400(){
             assertEquals("leap year", LeapYearSimple.leapyear(1992));
     }
+
+    public void typical_common_year_when_not_divisble_by_4(){
+        assertEquals("typical common year", LeapYearSimple.leapyear(2017));
+    }
 }
