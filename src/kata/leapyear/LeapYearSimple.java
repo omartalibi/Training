8ca87409ff64612408aa6f3e79544173ea5adab3
omartalibi <omar.talibi.96@gmail.com>
2018-01-17 20:33:57 +0100

    @Making test2 working
    typical_common_year_when_not_divisble_by_4

--- src/kata/leapyear/LeapYearSimple.java
+++ src/kata/leapyear/LeapYearSimple.java
@@ -5,6 +5,6 @@ public class LeapYearSimple {
     public static String leapyear(final int year){
         if(year % 4 == 0 && year % 100 != 0 && year % 400 != 0 )
             return "leap year";
-        return null;
+        return "typical common year";
     }
 }
