dfc991b604d509ac580e85c20b7cc71db2890b40
omartalibi <omar.talibi.96@gmail.com>
2018-01-17 09:04:01 +0100

    Adding project.

--- src/kata/fizzbuzz/FizzBuzzStream.java
+++ src/kata/fizzbuzz/FizzBuzzStream.java
@@ -9,12 +9,19 @@ public class FizzBuzzStream {
         List<Integer> numbers = new ArrayList<Integer>();
         numbers.add(number);
 
-        List<String> a = numbers.stream()
-                .filter(nb -> nb % 15 == 0)
-                .map(nb -> "fizzBuzz")
+        List<String> result;
+
+        result = numbers.stream()
+                .filter(nb -> nb % 3 == 0)
+                .map(nb -> "Fizz")
                 .collect(Collectors.toList());
 
-        return a.get(0);
+
+
+        if(result.isEmpty())
+            return String.valueOf(number);
+        else
+            return result.get(0);
     }
 
     }
