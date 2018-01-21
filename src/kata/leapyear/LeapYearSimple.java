520c61e5b9062f8e71da05ec0c64b05bc4ab444b
omartalibi <omar.talibi.96@gmail.com>
2018-01-21 17:51:06 +0100

    Small modif.

--- src/kata/leapyear/LeapYearSimple.java
+++ src/kata/leapyear/LeapYearSimple.java
@@ -1,16 +1,16 @@
 package kata.leapyear;
 
 public class LeapYearSimple {
-    static boolean isDivisble(long number, int divisor) {
-        return number % divisor == 0;
-    }
 
     public static boolean leapyear(final int year) {
-        boolean result = false;
         if (isDivisble(year,4)) {
             if(isDivisble(year,100) && isDivisble(year,400) || !isDivisble(year,100))
-                result=true;
+                return true;
         }
-        return result;
+        return false;
+    }
+
+    static boolean isDivisble(long number, int divisor) {
+        return number % divisor == 0;
     }
 }
