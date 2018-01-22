a95c3d8eabc343368550de810c1afb57ef0dd271
omartalibi <omar.talibi.96@gmail.com>
2018-01-22 22:18:10 +0100

    # SOLUTION : Typical leap year #

--- src/kata/leapyear/LeapYearSimple.java
+++ src/kata/leapyear/LeapYearSimple.java
@@ -2,9 +2,10 @@ package kata.leapyear;
 
 public class LeapYearSimple {
 
-    public static boolean leapyear(final int year) {
-
-        return true;
+    public static String leapyear(final int year) {
+        if(isDivisble(year, 4))
+            return "typical leap year";
+        return "other stuff";
     }
 
     static boolean isDivisble(long number, int divisor) {
