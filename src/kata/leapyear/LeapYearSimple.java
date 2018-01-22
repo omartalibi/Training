becf85225c7c97a4fff31c2f95a3af525353cf37
omartalibi <omar.talibi.96@gmail.com>
2018-01-22 22:21:45 +0100

    # SOLUTION : Atypical leap year #

--- src/kata/leapyear/LeapYearSimple.java
+++ src/kata/leapyear/LeapYearSimple.java
@@ -4,7 +4,10 @@ public class LeapYearSimple {
 
     public static String leapyear(final int year) {
         if(isDivisble(year, 4))
-            return "typical leap year";
+            if(isDivisble(year, 100) && isDivisble(year, 400))
+                return "atypical leap year";
+            else
+                return "typical leap year";
         return "other stuff";
     }
 
