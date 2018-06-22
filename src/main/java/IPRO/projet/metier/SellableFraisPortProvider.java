8b4e87cfb5d28a0e7077bef00d3b0bb6e778af67
omartalibi <omar.talibi.96@gmail.com>
2018-06-22 17:57:05 +0200

    #metier : FraisPortProvider && ReductionProvider

---
+++ src/main/java/IPRO/projet/metier/SellableFraisPortProvider.java
@@ -0,0 +1,14 @@
+package IPRO.projet.metier;
+
+import IPRO.projet.typage.FraisPort;
+import IPRO.projet.typage.Reference;
+
+public class SellableFraisPortProvider {
+    public static FraisPort getFraisPort(Reference reference){
+        /** FROM FILE */
+
+
+
+        return null;
+    }
+}
---
+++ src/main/java/IPRO/projet/metier/SellableReductionProvider.java
@@ -0,0 +1,20 @@
+package IPRO.projet.metier;
+
+import IPRO.projet.typage.Reduction;
+import IPRO.projet.typage.Reference;
+
+public class SellableReductionProvider {
+
+    public static boolean hasReduction(Reference reference){
+
+
+        return false;
+    }
+
+    public static Reduction getReduction(Reference reference){
+
+
+        return null;
+
+    }
+}
